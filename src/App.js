02cb3689edb3c38bc05e5b416a8216a998a32a25
SriRam <sramaram@rythmos.com>
2020-07-04 14:05:39 +0530

    add collection-preview and -item components and styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
-import { Link, Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shoppage.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
