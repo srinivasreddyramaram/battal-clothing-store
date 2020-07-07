4a38bc71ac133e685be3305535040df1ca79d36a
SriRam <sramaram@rythmos.com>
2020-07-07 11:35:13 +0530

    add sign-in page with form-input and custom-button components, styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HomePage } from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shoppage.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up-page/sign-in-sign-up-page.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
